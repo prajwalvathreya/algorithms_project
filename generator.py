294f22f74be906e8dc15097c9f011aeee7fbafb8
prajwalathreya <athreya.p@northeastern.edu>
2024-04-02 16:58:32 -0400

    added documentation

--- generator.py
+++ generator.py
@@ -2,20 +2,25 @@ import random
 import numpy as np
 
 class Graph:
+    """Graph class to represent the maze."""
     def __init__(self):
+        """Initialize the Graph object with vertices and edges."""
         self.vertices = {}
         self.edges = []
 
     def add_vertex(self, vertex):
+        """Add a vertex to the graph."""
         if vertex not in self.vertices:
             self.vertices[vertex] = []
 
     def add_edge(self, vertex1, vertex2, weight):
+        """Add an edge to the graph between two vertices."""
         self.vertices[vertex1].append((vertex2, weight))
         self.vertices[vertex2].append((vertex1, weight))
         self.edges.append((weight, vertex1, vertex2))
 
 def kruskal_maze(rows, cols):
+    """Generate a maze using Kruskal's algorithm."""
     graph = Graph()
 
     # Add all vertices to the graph
@@ -55,22 +60,35 @@ def kruskal_maze(rows, cols):
     return maze
 
 def generate_matrix(maze, rows, cols):
+    """
+    Generate a matrix representation of the maze.
     
+    Args:
+        maze (list): A list of edges representing the maze.
+        rows (int): The number of rows in the maze.
+        cols (int): The number of columns in the maze.
+    
+    Returns:
+        np.array: A matrix representation of the maze.
+    """
+    
+    # Initialize a matrix with walls represented as 'X'
     maze_cells = [['X' for _ in range(2 * cols + 1)] for _ in range(2 * rows + 1)]
     
+    # Add paths between cells based on the maze
     for (v1, v2) in maze:
         row1, col1 = v1
         row2, col2 = v2
-        maze_cells[2*row1+1][2*col1+1] = ' '
-        maze_cells[row1+row2+1][col1+col2+1] = ' '
+        maze_cells[2*row1+1][2*col1+1] = ' '  # Cell
+        maze_cells[row1+row2+1][col1+col2+1] = ' '  # Path between cells
     
+    # Convert maze representation to matrix
     matrix = np.zeros((2 * rows + 1, 2 * cols + 1))
-
-    for i in range (len(maze_cells)):
+    for i in range(len(maze_cells)):
         row = maze_cells[i]
         for j in range(len(row)):
             element = row[j]
             if element == 'X':
-                matrix[i][j] = 8
+                matrix[i][j] = 8  # Represent walls as 8
     
     return matrix
--- solver.py
+++ solver.py
@@ -3,14 +3,23 @@ from generator import *
 import random
 
 def bfs(matrix_maze, start, end):
-
+    """
+    Perform breadth-first search to find a path from start to end in the maze represented by matrix_maze.
+    
+    Args:
+        matrix_maze (list): A matrix representation of the maze.
+        start (tuple): The starting vertex.
+        end (tuple): The ending vertex.
+    
+    Returns:
+        list: A list of vertices representing the path from start to end.
+    """
     visited = set()
     queue = deque()
     path = []
     queue.append(start)
 
     while queue:
-
         current = queue.popleft()
         
         if current == end:
@@ -31,6 +40,17 @@ def bfs(matrix_maze, start, end):
     return path
 
 def get_neighbors(vertex):
+
+    """
+    Get neighboring vertices of a given vertex.
+    
+    Args:
+        vertex (tuple): A tuple representing the vertex.
+
+    Returns:
+        list: A list of neighboring vertices.
+
+    """
     row, col = vertex
     neighbors = []
     if row > 0:
@@ -49,32 +69,32 @@ cols = 10
 maze = kruskal_maze(rows, cols)
 
 while True:
-
+    # Choose random start and end points
     all_vertices = [item for sublist in maze for item in sublist]
-
     start = random.choice(all_vertices)
     end = random.choice(all_vertices)
     
+    # Ensure start and end are different
     if start != end:
-
         matrix = generate_matrix(maze, rows, cols)
         path = bfs(matrix, start, end)
-        
+
+        # If a valid path is found, print the matrix and path, then break the loop
         if len(path) != 0:
             print(matrix)
             print("Solution Exists")
             print(path)
             break
 
+# Update matrix to highlight the path
 updated_matrix = matrix.copy()
 
 for i in range(len(matrix)):
     row = matrix[i]
     for j in range(len(row)):
         if (i, j) in path:
-            updated_matrix[i][j] = 9
+            updated_matrix[i][j] = 9  # Highlight the path
         else:
             updated_matrix[i][j] = 0
-
 
 print(updated_matrix)
