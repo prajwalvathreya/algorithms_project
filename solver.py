b26355ea42399f62ff38befe64d631e58126e4d4
Navneet Parab <navneetparabb20@gmail.com>
2024-04-05 13:45:28 -0400

    Added function for DFS implementation. Changed the matrix value of highlighted path from 9 to 1 for better visibility of the path.

--- solver.py
+++ solver.py
@@ -39,6 +39,43 @@ def bfs(matrix_maze, start, end):
 
     return path
 
+def dfs(matrix_maze, start, end):
+    """
+    Perform depth-first search to find a path from start to end in the maze represented by matrix_maze.
+
+    Args:
+        matrix_maze (list): A matrix representation of the maze.
+        start (tuple): The starting vertex.
+        end (tuple): The ending vertex.
+
+    Returns:
+        list: A list of vertices representing the path from start to end.
+    """
+    visited = set()
+    stack = []
+    path = []
+    stack.append(start)
+
+    while stack:
+        current = stack.pop()
+
+        if current == end:
+            path.append(current)
+            return path
+
+        if current in visited:
+            continue
+
+        visited.add(current)
+
+        for neighbor in get_neighbors(current):
+            row, col = neighbor
+            if matrix_maze[row][col] == 0:
+                stack.append(neighbor)
+                path.append(neighbor)
+
+    return path
+
 def get_neighbors(vertex):
 
     """
@@ -77,24 +114,46 @@ while True:
     # Ensure start and end are different
     if start != end:
         matrix = generate_matrix(maze, rows, cols)
-        path = bfs(matrix, start, end)
+        path_bfs = bfs(matrix, start, end)
+        path_dfs = dfs(matrix, start, end)
+
 
         # If a valid path is found, print the matrix and path, then break the loop
-        if len(path) != 0:
+        if len(path_bfs) != 0:
             print(matrix)
-            print("Solution Exists")
-            print(path)
+            print("Solution Exists for BFS")
+            print(path_bfs)
+        #    break
+
+        if len(path_dfs) != 0:
+            #print(matrix)
+            print("Solution Exists for DFS")
+            print(path_dfs)
             break
 
 # Update matrix to highlight the path
-updated_matrix = matrix.copy()
+updated_matrix_bfs = matrix.copy()
+updated_matrix_dfs = matrix.copy()
+
 
 for i in range(len(matrix)):
     row = matrix[i]
     for j in range(len(row)):
-        if (i, j) in path:
-            updated_matrix[i][j] = 9  # Highlight the path
+        if (i, j) in path_bfs:
+            updated_matrix_bfs[i][j] = 1  # Highlight the path
         else:
-            updated_matrix[i][j] = 0
+            updated_matrix_bfs[i][j] = 0
 
-print(updated_matrix)
+print(updated_matrix_bfs)
+
+
+for i in range(len(matrix)):
+    row = matrix[i]
+    for j in range(len(row)):
+        if (i, j) in path_dfs:
+            updated_matrix_dfs[i][j] = 1  # Highlight the path
+        else:
+            updated_matrix_dfs[i][j] = 0
+
+print(updated_matrix_dfs)
+
